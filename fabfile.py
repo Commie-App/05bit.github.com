333a016874f12f9c33f6f963fe9e6e895117f609
Alexey Kinyov <rudy@05bit.com>
2012-03-09 00:07:18 +0300

    initial commit

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+from fabric.api import *
+import os
+
+
+PROJECT_DIR = os.path.realpath(os.path.dirname(__file__))
+        
+def build():
+    local('mynt -f src build')
+    local('rsync -r build/ ./')
+
+def deploy():
+    pass
+
+def runserver():
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+    import BaseHTTPServer
+    server_address = ('localhost', 8000)
+    httpd = BaseHTTPServer.HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print "Running server on http://%s:%s ..." % server_address
+    httpd.serve_forever()
